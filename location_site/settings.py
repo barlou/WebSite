e7bee129d7199f0c3f6b50a65b8f4480a21e5dfd
Louis Barillon <57702626+barlou@users.noreply.github.com>
2023-02-23 17:46:18 +0100

    [Fix] Delete static_root

--- location_site/settings.py
+++ location_site/settings.py
@@ -165,8 +165,6 @@ STATICFILES_DIRS = [
 
 STATIC_URL = '/static/'
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
