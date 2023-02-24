b8a2ef2d1bcc8d5bd0031af9a42124fadbd64d23
Louis Barillon <57702626+barlou@users.noreply.github.com>
2023-02-24 15:45:00 +0100

    [Fix] Empty list staticfiles_dirs

--- location_site/settings.py
+++ location_site/settings.py
@@ -159,7 +159,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATICFILES_DIRS = [
-    BASE_DIR / "static",
+    # BASE_DIR / "static",
 ]
 
 AWS_ACCESS_KEY_ID = 'AKIA2VQFQTMXFCFJUPCM'
